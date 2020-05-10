0d0cc714796f6d49f3ad877cf6daa205e0b7dc20
maliss <brancourt@hotmail.com>
2020-05-10 13:27:02 -0400

    validation

--- src/tickets/dto/create-ticket.dto.ts
+++ src/tickets/dto/create-ticket.dto.ts
@@ -1,7 +1,16 @@
+import { IsNotEmpty, IsString, IsInt, IsNumber } from 'class-validator';
+
 export class CreateTicketDTO {
-    readonly id: string;
-    readonly name: string;
-    readonly qty: number;
-    readonly price: number;
-  }
-  
+  @IsNotEmpty()
+  @IsString()
+  readonly id: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+  @IsNotEmpty()
+  @IsInt()
+  readonly qty: number;
+  @IsNotEmpty()
+  @IsNumber()
+  readonly price: number;
+}
