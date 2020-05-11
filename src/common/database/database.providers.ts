2172658861374aaef6e0e2597c750fdf3e71006c
maliss <brancourt@hotmail.com>
2020-05-10 22:40:44 -0400

    database

--- src/common/database/database.providers.ts
+++ src/common/database/database.providers.ts
@@ -7,11 +7,18 @@ export const databaseProviders = [
     useFactory: async () => {
       const sequelize = new Sequelize({
         dialect: 'postgres',
+        protocol: 'postgres',
         host: 'ec2-54-81-37-115.compute-1.amazonaws.com',
         port: 5432,
         username: 'uxhvbbxpfpneeb',
         password: '9fa531b4a1139b159975921c13f61d39411eefbc43c6624d9637fff61b7d1850',
         database: 'd7lau3upsk9e6f',
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
       });
       sequelize.addModels([Tickets]);
       await sequelize.sync();
